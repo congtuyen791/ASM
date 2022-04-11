9a60f9d17deb4360a400537d82123ecc194b7f46
Tuyền Vũ <you@example.com>
2022-04-11 22:23:35 +0700

    base64

--- src/app/layouts/admin/admin-product-form/admin-product-form.component.ts
+++ src/app/layouts/admin/admin-product-form/admin-product-form.component.ts
@@ -11,16 +11,17 @@ import { ProductService } from 'src/app/services/product.service';
 export class AdminProductFormComponent implements OnInit {
   productForm: FormGroup;
   id: string | undefined;
+  avatarBase64: any;
   product: any;
   constructor(
     private productService: ProductService,
     private router: Router,
     private activateRoute: ActivatedRoute
-  ) { 
+  ) {
     this.productForm = new FormGroup({
       id: new FormControl(null),
-      name: new FormControl('',[Validators.required, Validators.minLength(6), Validators.maxLength(50)]),
-      image_url: new FormControl('', Validators.required),
+      name: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(50)]),
+      image_url: new FormControl(''),
       price: new FormControl(0, Validators.required),
       status: new FormControl(1),
       desc: new FormControl('', Validators.required)
@@ -30,17 +31,15 @@ export class AdminProductFormComponent implements OnInit {
   ngOnInit(): void {
     this.id = this.activateRoute.snapshot.params['id'];
     console.log(this.id);
-    
+
     if (this.id) {
       this.productService.getProduct(this.id).subscribe(data => {
-        this.product = data;
+        // this.product = data;
         this.productForm.setValue(data);
         console.log(data);
-        
       });
-    }else{
+    } else {
       this.product = {
-        // id: 1,
         name: '',
         desc: '',
         price: 0,
@@ -49,19 +48,45 @@ export class AdminProductFormComponent implements OnInit {
       }
     }
   }
-  onSubmit(data: any){
+  onSubmit(data: any) {
+    const submitData = {
+      ...data,
+      image_url: this.avatarBase64
+    }
     if (this.id) {
-      return this.productService.updateProduct(this.id, data).subscribe((obj) => {
+      return this.productService.updateProduct(this.id, submitData).subscribe((obj) => {
         this.router.navigate(['admin/phones', this.id]);
         alert('Sửa thành công');
       })
     }
-    return this.productService.createProduct(data).subscribe((obj) => {
+    return this.productService.createProduct(submitData).subscribe((obj) => {
       this.router.navigate(['admin/phones']);
       alert('Thêm mới thành công');
     });
-    
-    
+  }
+  resultString(e: any) {
+    if (e && e.target && typeof e.target.result == 'string') {
+      return e.target.result;
+    }
+
+    return '';
+  }
+
+  changeAvatar(event: any) {
+    const arrayImageTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+    const file = event.target.files[0];
+    if (file.size > 3000000) {
+      return alert('Kích thước file quá lớn');
+    } else if (!arrayImageTypes.includes(file.type)) {
+      return alert('Kiểu dữ liệu không phù hợp');
+    }
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      this.avatarBase64 = e.target?.result;
+      const image = new Image();
+      image.src = this.resultString(e);
+    }
+    reader.readAsDataURL(file);
   }
 
 }
